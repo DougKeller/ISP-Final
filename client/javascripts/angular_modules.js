c80eac056f41f31c6f823d057775d3a0ce7579a6
Doug Keller <dkeller@synergydatasystems.com>
2016-12-09 02:14:32 -0500

    Reduce to only the files needed to run

--- client/javascripts/angular_modules.js
+++
@@ -1,9 +0,0 @@
-angular.module('calendar', [
-  'templates',
-  'ngAnimate',
-  'angular-jwt',
-  'ui.router',
-  'angularMoment',
-  'ui.bootstrap',
-  'ui.bootstrap.datetimepicker'
-]);
--- client/javascripts/filters/sref_filter.js
+++
@@ -1,5 +0,0 @@
-angular.module('calendar').filter('sref', ['sref',
-  function(sref) {
-    return sref;
-  }
-]);
